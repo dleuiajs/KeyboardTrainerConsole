f4d7faadc79050267d7f1bd89a83f2c9ade394a0
dleuiajs <jduejdb322@gmail.com>
2022-12-05 12:55:02 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SchoolTimeTable
+{
+    internal class Program
+    {
+        static string textNeed = "loshara";
+        static string textEntered;
+
+        static void Main()
+        {
+            EnterText();
+        }
+
+        static void EnterText()
+        {
+            Console.WriteLine("Enter " + "'" + textNeed + "'");
+            textEntered = Console.ReadLine();
+            if (textEntered == textNeed)
+            {
+                Console.WriteLine("Good job!");
+                EnterText();
+            }
+        }
+    }
+}
